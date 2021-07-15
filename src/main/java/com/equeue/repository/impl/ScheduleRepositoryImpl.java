f2fade2bd7ff762d1f362d241c94b2fc3d2e79cb
AxelNordov <iq@ua.fm>
2021-07-15 12:25:46 +0300

    fix ScheduleRepository

--- src/main/java/com/equeue/repository/impl/ScheduleRepositoryImpl.java
+++ src/main/java/com/equeue/repository/impl/ScheduleRepositoryImpl.java
@@ -1,26 +1,34 @@
 package com.equeue.repository.impl;
 
 import com.equeue.entity.Schedule;
-import org.springframework.stereotype.Repository;
+import com.equeue.repository.ProviderRepository;
 import com.equeue.repository.ScheduleRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.TreeMap;
 import java.util.stream.Collectors;
 
 @Repository
 public class ScheduleRepositoryImpl implements ScheduleRepository {
 
+    @Autowired
+    ProviderRepository providerRepository;
+
     Map<Long, Map<Integer, Schedule>> scheduleMap = new HashMap<>();
 
     @Override
     public Schedule save(Schedule schedule) {
-        final Long userId = schedule.getProvider().getId();
+        final Long providerId = schedule.getProvider().getId();
         final Integer dayOfWeek = schedule.getDayOfWeek();
-        scheduleMap.computeIfAbsent(userId, k -> new HashMap<>());
-        scheduleMap.get(userId).put(dayOfWeek, schedule);
-        return scheduleMap.get(userId).get(dayOfWeek);
+        scheduleMap.computeIfAbsent(providerId, k -> new TreeMap<>());
+        Map<Integer, Schedule> providerScheduleMap = scheduleMap.get(providerId);
+        providerScheduleMap.put(dayOfWeek, schedule);
+        providerRepository.findById(providerId).setScheduleMap(providerScheduleMap);
+        return providerScheduleMap.get(dayOfWeek);
     }
 
     @Override
