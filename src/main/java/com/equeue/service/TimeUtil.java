5355ee8d4fbcf13d7e7f113d03e1ec3937a73a1f
AxelNordov <iq@ua.fm>
2021-07-23 00:47:47 +0300

    update TimeUtil
    add new DateTimeFormatters to fix February 29

--- src/main/java/com/equeue/service/TimeUtil.java
+++ src/main/java/com/equeue/service/TimeUtil.java
@@ -6,31 +6,61 @@ import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.chrono.IsoChronology;
 import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+import java.time.format.ResolverStyle;
+import java.time.format.SignStyle;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
 
+import static java.time.temporal.ChronoField.*;
+
 public class TimeUtil {
 
-    public static final String TIME_PATTERN = "H:mm";
+    public static final String TIME_PATTERN_PARSE = "H:mm";
+    public static final String TIME_PATTERN = "HH:mm";
+    public static final String DATE_PATTERN_PARSE = "d.M.yyyy";
     public static final String DATE_PATTERN = "dd.MM.yyyy";
-    public static final String DATE_TIME_PATTERN = "dd.MM.yyyy H:mm";
+    public static final String DATE_TIME_PATTERN_PARSE = "d.M.yyyy H:mm";
+    public static final String DATE_TIME_PATTERN = "dd.MM.yyyy HH:mm";
+
+    public static final DateTimeFormatter DATE_FORMATTER_PARSE = new DateTimeFormatterBuilder()
+            .appendValue(DAY_OF_MONTH, 1, 2, SignStyle.NORMAL)
+            .appendLiteral('.')
+            .appendValue(MONTH_OF_YEAR, 1, 2, SignStyle.NORMAL)
+            .appendLiteral('.')
+            .appendValue(YEAR, 4, 4, SignStyle.EXCEEDS_PAD)
+            .toFormatter().withResolverStyle(ResolverStyle.STRICT).withChronology(IsoChronology.INSTANCE);
+
+    public static final DateTimeFormatter DATE_TIME_FORMATTER_PARSE = new DateTimeFormatterBuilder()
+            .appendValue(DAY_OF_MONTH, 1, 2, SignStyle.NORMAL)
+            .appendLiteral('.')
+            .appendValue(MONTH_OF_YEAR, 1, 2, SignStyle.NORMAL)
+            .appendLiteral('.')
+            .appendValue(YEAR, 4, 4, SignStyle.EXCEEDS_PAD)
+            .appendLiteral(' ')
+            .appendValue(HOUR_OF_DAY, 1, 2, SignStyle.NORMAL)
+            .appendLiteral(':')
+            .appendValue(MINUTE_OF_HOUR, 2)
+            .toFormatter().withResolverStyle(ResolverStyle.STRICT).withChronology(IsoChronology.INSTANCE);
 
     private TimeUtil() {
         throw new IllegalStateException("Utility class");
     }
 
     public static LocalTime getTimeFromString(String stringTime) {
-        return LocalTime.parse(stringTime, DateTimeFormatter.ofPattern(TIME_PATTERN));
+        return LocalTime.parse(stringTime,
+                DateTimeFormatter.ofPattern(TIME_PATTERN_PARSE));
     }
 
     public static LocalDate getDateFromString(String stringDate) {
-        return LocalDate.parse(stringDate, DateTimeFormatter.ofPattern(DATE_PATTERN));
+        return LocalDate.parse(stringDate, DATE_FORMATTER_PARSE);
     }
 
     public static LocalDateTime getDateTimeFromString(String stringDateTime) {
-        return LocalDateTime.parse(stringDateTime, DateTimeFormatter.ofPattern(DATE_TIME_PATTERN));
+        return LocalDateTime.parse(stringDateTime, DATE_TIME_FORMATTER_PARSE);
     }
 
     public static String getStringFromTime(LocalTime localTime) {
